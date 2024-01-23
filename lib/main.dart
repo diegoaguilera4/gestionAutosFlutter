90c5fcaa5a2ba1e3455bd70be3252d07b8c749d7
Diego Aguilera <diegoaguilera4@icloud.com>
2024-01-23 17:51:52 -0300

    avance en enviar registro

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:qrscan/qrscan.dart' as scanner;
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(MyApp());
@@ -26,16 +27,43 @@ class _MyAppState extends State<MyApp> {
 
       // Extrae la parte de la cadena después de "RUN=" hasta el carácter "-"
       String extractedPart = cameraScanResult.substring(startIndex + 4, endIndex);
-
+      print("RUN: $extractedPart");
       setState(() {
         qrValue = extractedPart;
       });
+
+      // Realiza la solicitud GET
+      await performGetRequest(extractedPart);
+
+      // Realiza la solicitud POST
+      await performPostRequest(extractedPart);
+
+      
+
+      // Actualiza el estado de la aplicación
     } else {
       // Si la cadena escaneada no contiene "RUN=", puedes manejarlo de la manera que desees
       setState(() {
         qrValue = "No se encontró el formato esperado";
       });
     }
+  }
+
+  Future<void> performGetRequest(String rut) async {
+    // Aquí realiza tu solicitud GET al servidor con el rut
+    // Por ejemplo:
+    String apiUrl = "http://172.28.199.144:3000/personas/obtenerRut/$rut";
+    http.Response response = await http.get(Uri.parse(apiUrl));
+    print(response.body);
+  }
+
+  Future<void> performPostRequest(String rut) async {
+    // Aquí realiza tu solicitud POST al servidor con el rut
+    // Por ejemplo:
+    String apiUrl = "http://172.28.199.144:3000/registros/agregar";
+    Map<String, String> body = {"rut": rut};
+    http.Response response = await http.post(Uri.parse(apiUrl), body: body);
+    print(response.body);
   }
 
   @override
